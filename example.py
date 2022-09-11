e1cbcf5cdca4efd09b4980aab66b5146551ac886
clipssender <100996857+clipssender31@users.noreply.github.com>
2022-09-11 13:17:40 +1000

    Star 😋

---
+++ example.py
@@ -0,0 +1,5 @@
+# 60 stars for a version using yolov5 and solves locally 😙
+from solver.solver import Solver
+
+print(Solver(UID="Get your free UID from https://nocaptchaai.com/!", APIKEY="Get a Free API key from https://nocaptchaai.com/!", site_key="4c672d35-0701-42b2-88c3-78380b0db560", website_url="discord.com", proxyless=True, endpoint_url="https://free.nocaptchaai.com/api/solve").SolveCaptcha())
+# Solver().SolveCaptcha() Returns a string
---
+++ setup.py
@@ -0,0 +1,3 @@
+import os
+os.system('pip install -qr requirements.txt')
+print('Successfully installed all packages')
